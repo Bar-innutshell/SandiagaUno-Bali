ec4b9415e7932d81e70d91f15a53fe87d05b0642
Pfftz <Kalixpfft+github@protonmail.com>
2024-09-05 08:57:17 +0700

    feat: Update goblin_corrupt.tscn and goblin_corrupt.gd for enemy behavior improvements

--- Scripts/Enemies/goblin_corrupt.gd
+++ Scripts/Enemies/goblin_corrupt.gd
@@ -4,15 +4,17 @@ class_name GoblinEnemy
 
 @export var speed = 30
 var dir: Vector2
+var direction: int
 const gravity = 900
-var knockback_force = 200
+var knockback_force = -50
 var is_roaming: bool = true
 
 @export var health_amount : int = 3
 @export var damage_amount : int = 1
-var is_goblin_chase: bool
+var is_goblin_chase: bool = true
 
 var player: CharacterBody2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready():
 	is_goblin_chase = true
@@ -21,16 +23,25 @@ func _process(delta):
 	if !is_on_floor():
 		velocity.y += gravity * delta
 		velocity.x = 0
-	move(delta)
+	
+	player = GameManager.playerBody
+	if player != null:
+		move(delta)
 	handle_animation()
 
 func move(delta):
-	if is_goblin_chase:
-		player = GameManager.playerBody
-		velocity = position.direction_to(player.position) * speed
-		dir.x = abs(velocity.x) / velocity.x
-	elif !is_goblin_chase:
-		velocity += dir * speed * delta
+	if health_amount > 0:
+		if !is_goblin_chase:
+			velocity += dir * speed * delta
+		elif is_goblin_chase:
+			if player != null:
+				var dir_to_player = position.direction_to(player.position) * speed
+				velocity.x = dir_to_player.x
+				dir.x = abs(velocity.x) / velocity.x
+	elif health_amount <= 0:
+		velocity.x = 0
+		if !animated_sprite.is_playing() or animated_sprite.animation != "die":
+			animated_sprite.play("die")
 	move_and_slide()
 
 func _on_timer_timeout():
@@ -40,12 +51,11 @@ func _on_timer_timeout():
 		velocity.x = 0
 
 func handle_animation():
-	var animated_sprite = $AnimatedSprite2D
-	animated_sprite.play("fly")
+	animated_sprite.play("idle")
 	if dir.x == -1:
-		animated_sprite.flip_h = true
-	elif dir.x == 1:
 		animated_sprite.flip_h = false
+	elif dir.x == 1:
+		animated_sprite.flip_h = true
 
 func choose(array):
 	array.shuffle()
@@ -56,7 +66,8 @@ func _on_hurtbox_area_entered(area):
 	if area.get_parent().has_method("get_damage_amount"):
 		var node = area.get_parent() as Node
 		health_amount -= node.damage_amount
+		var knockback_dir = position.direction_to(player.global_position) * knockback_force
+		velocity.x = knockback_dir.x
 		print("Health amount: ", health_amount)
 		if health_amount <= 0:
-			get_parent().add_child()
 			queue_free()
